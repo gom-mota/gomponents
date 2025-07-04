f9f632d390abd2dbc89a9f7a6399322964438fb4
Gabriel Mota <gom.mota@gmail.com>
2025-07-04 08:07:43 -0300

    refact: responsive layout improves

--- src/common/Sidebar/index.js
+++ src/common/Sidebar/index.js
@@ -20,58 +20,97 @@ export const handleSetActiveNavItem = () => {
 }
 
 const Sidebar = () => {
+	window.handleClickNavItem = (route) => {
+		const sidebar = document.getElementById('sidebar')
+		navigateToRoute(route)
+
+		if (sidebar && sidebar.classList.contains('mobile'))
+			sidebar.classList.remove('mobile')
+	}
+
 	const render = /*html*/ `
-	<div class='sidebar-container'>
-        <header>
-            <div>
-                <h1>GOMUI</h1>
-                <span>web components lib</span>
-            </div>
+    <div>
+        <div class='sidebar-container' id='sidebar-container'>
+           <header>
+                <div>
+                    <h1>GOMUI</h1>
+                    <span>web components lib</span>
+                </div>
 
-            <a href="https://github.com/gom-mota/gomponents" target="_blank">
-                <ion-icon name="logo-github"></ion-icon>
-            </a>
-        </header>
+                <a href="https://github.com/gom-mota/gomponents" target="_blank">
+                    <ion-icon name="logo-github"></ion-icon>
+                </a>
+            </header>
+            
+            <nav>
+                <ul>
+                    <li>Ponto de Partida</li>
+
+                    <ul>
+                        ${Object.entries(ROUTES)
+							.filter(([_, { nav }]) => nav === true)
+							.map(
+								([route, { name, label }]) => /*html*/ `
+                                    <li 
+                                        id="nav-${name.toLowerCase()}"
+                                        onclick="handleClickNavItem('${route}')">
+                                            ${label}
+                                    </li>
+                                `
+							)
+							.join('')}
+                    </ul>
+
+                    <li>Componentes</li>
+
+                    <ul>
+                        ${Object.keys(components.expose)
+							.map(
+								(name) => /*html*/ `
+                                    <li id="nav-${name}" onclick="handleClickNavItem('/component/${name}')">
+                                        ${name}
+                                    </li>
+                                `
+							)
+							.join('')}
+                    </ul>
+                </ul>
+            </nav>
+        </div>
         
-        <nav>
-            <ul>
-                <li>Ponto de Partida</li>
+        <div id="sidebar-overlay"></div>
 
-                <ul>
-                    ${Object.entries(ROUTES)
-						.filter(([_, { nav }]) => nav === true)
-						.map(
-							([route, { name, label }]) => /*html*/ `
-                                <li 
-                                    id="nav-${name.toLowerCase()}"
-                                    onclick="navigateToRoute('${route}')">
-                                        ${label}
-                                </li>
-                            `
-						)
-						.join('')}
-                </ul>
+        <div class='header-top'>
+            <div class='content'>
+                <div class='logo-container'>
+                    <h1>GOMUI</h1>
+                    <span>web components lib</span>
+                </div>
 
-                <li>Componentes</li>
+                <div class='buttons-container'>
+                    <a href="https://github.com/gom-mota/gomponents" target="_blank">
+                        <ion-icon name="logo-github"></ion-icon>
+                    </a>
 
-                <ul>
-                    ${Object.keys(components.expose)
-						.map(
-							(name) => /*html*/ `
-                                <li id="nav-${name}" onclick="navigateToRoute('/component/${name}')">
-                                    ${name}
-                                </li>
-                            `
-						)
-						.join('')}
-                </ul>
-            </ul>
-        </nav>
-	</div>
+                    <button id='toggle-sidebar'> <ion-icon name="menu-outline"></ion-icon></button>
+                </div>
+            </div>
+        </div>
+    </div>
     `
 
 	const after_render = () => {
-		handleSetActiveNavItem()
+		const sidebar = document.getElementById('sidebar')
+		const sidebarOverlay = document.getElementById('sidebar-overlay')
+		const toggleSidebarButton = document.getElementById('toggle-sidebar')
+
+		toggleSidebarButton.addEventListener('click', () => {
+			if (sidebar) sidebar.classList.add('mobile')
+		})
+
+		sidebarOverlay.addEventListener('click', () => {
+			sidebar.classList.remove('mobile')
+		})
 	}
 
 	return { render, after_render }
